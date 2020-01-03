9d6304ee4d0d289685df45207177a491e3aa060b
nortoha <39999692+nortoha@users.noreply.github.com>
2020-01-03 15:42:43 -0600

    Added teacher comments and grading
    Good job!  Your grade is a 10 out of 10 points. But please review my notes here and try to incorporate those corrections next time.

--- Tests/TestQuestion15.java
+++ Tests/TestQuestion15.java
@@ -1,13 +1,18 @@
-
+// Good job!  Your grade is a 10 out of 10 points. But please review my notes here and try to incorporate those corrections next time.
 public class TestQuestion15 {
 	
 	public static void main(String[] args) {
 		
+		// the curly brace here on line 7 is not necessary.
 		int numTimes=5; {
+			
+			// this line should not be indented. make sure this variable starts with a lowercase letter testQuestion
 			String TestQuestion = "1 1 1 1 1"; 
-					
+			
+			// since this for loop is just one line long its ok to not have curly braces, but its not recommened to leave them off!
 			for(int counter=0; counter < numTimes; counter++) 
 				System.out.println(TestQuestion);
+		// the curly brace here on line 16 is not necessary.
 		}			
 			
 			
